0fa8c3cc0a6976209a7d85a51d2952b1720662cd
seok9924 <41310927+seok9924@users.noreply.github.com>
2023-03-01 06:53:02 +0900

    조금씩 다시 하는중
    god god god

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Post
 # Create your views here.
+from django.views.generic import ListView,CreateView,DetailView,DeleteView
 
 
-def index(request):
-    posts=Post.objects.all()
+# def index(request):
+#     posts=Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',{
+#             'posts' :posts
+#                          },
+#     )
 
-    return render(
-        request,
-        'blog/index.html',{
-            'post' :posts
-                         },
-    )
+# fbv 로 만든 인덱스
+# def single_post_page(request,pk) :
+#     post=Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+#     )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+#     template_name = 'blog/index.html'
+# 장고가 제공하는 ListView 는 모델명 뒤에 _list가 붙은 html 파일을 기본 템플릿으로 사용
+# html 내에서도 post_list로 받음
+
+
+
+class PostDetail(DetailView):
+    model=Post
